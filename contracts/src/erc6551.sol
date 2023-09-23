7028f7a25066735145939fa55d8041cfa073e3c8
FudgyDRS <yetimonsters@gmail.com>
2023-09-23 17:43:38 -0400

    stuff

--- contracts/src/erc6551.sol
+++ contracts/src/erc6551.sol
@@ -25,19 +25,58 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executabl
 
     receive() external payable {}
 
+    //function internal
+
     function execute(
         address to,
         uint256 value,
         bytes calldata data,
         uint256 operation
     ) external payable returns (bytes memory result) {
-        require(_isValidSigner(msg.sender), "Invalid signer"); // fk that use ECDSA proof
+        /**
+          * execution flow should be
+          * call target contract << call 4337 contracts << call 6551 contract << EntryPoint
+          * this means that the owner of the 4337 is the 6551
+          * 4337 ecdsa recovery >> 6551
+          * that means 6551 needs to encode and sign it's input data;
+          * maybe paid for by biconmoy, maybe not idc
+          *
+          * data order: blah, blah, slot, blah, calldata
+          * calldata: signature, transaction (userop)
+          * needs state to be in the signature to protect against sybil attacks
+
+         */
+        // need to make sure the size of the calldata
+        // calldata = (size + 32) - (4 + 32*4) => size + 32 >= 229
+        // first 132 are garbage
+        // next 32 is data length
+        // next 32 + 65 is signature
+        // if next 32 is not 0, 32 + ? is userop
+        // means size must be at least 132 + 97
+        uint256 size = msg.data.length;
+        require(size >= 229, "Invalid data length");
+        bytes memory signature_ = msg.data[197:229];
+        //bytes memory userop = msg.data[229:size];
+
+        bytes32 hash_ = keccak256(state, msg.data[229:size])
+        bytes memory signature_ = msg.data[133:197];
+        (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(hash_.toEthSignedMessageHash(), signature_);
+        if (error != ECDSA.RecoverError.NoError) {
+            revert BadSignature();
+        }
+
+        if(recovered != account_) {
+            revert InvalidSignature(account_, recovered);
+        }
+
+
+        revert _isValidSigner(recovered, "Invalid signer");
         require(operation == 0, "Only call operations are supported");
 
         ++state;
 
         bool success;
-        (success, result) = to.call{value: value}(data);
+        (success, result) = to.call{value: value}(msg.data[229:size]);
 
         if (!success) {
             assembly {
@@ -46,9 +85,24 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executabl
         }
     }
 
-    function isValidSigner(address signer, bytes calldata) external view returns (bytes4) {
-        if (_isValidSigner(signer)) {
+    function isValidSigner(address signer, bytes calldata data) external view returns (bytes4) {
+        if (_isValidSigner(signer)) { // see already have the bytes to have a sig
             return IERC6551Account.isValidSigner.selector;
+        }
+        uint256 size = msg.data.length;
+        require(size >= 64, "Invalid data length");
+        bytes memory signature_ = msg.data[647:229];
+        //bytes memory userop = msg.data[229:size];
+
+        bytes32 hash_ = keccak256(state, msg.data[229:size])
+        bytes memory signature_ = msg.data[133:197];
+        (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(hash_.toEthSignedMessageHash(), signature_);
+        if (error != ECDSA.RecoverError.NoError) {
+            revert BadSignature();
+        }
+
+        if(recovered != account_) {
+            revert InvalidSignature(account_, recovered);
         }
 
         return bytes4(0);
@@ -100,6 +154,18 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executabl
     }
 
     function _isValidSigner(address signer) internal view returns (bool) {
+        /**
+          * execution flow should be
+          * call target contract << call 4337 contracts << call 6551 contract << EntryPoint
+          * this means that the owner of the 4337 is the 6551
+          * 4337 ecdsa recovery >> 6551
+          * that means 6551 needs to encode and sign it's input data;
+          * maybe paid for by biconmoy, maybe not idc
+          *
+          * data order: blah, blah, slot, blah, calldata
+          * needs state to be in the signature to protect against sybil attacks
+
+         */
         return signer == owner();
     }
 }
